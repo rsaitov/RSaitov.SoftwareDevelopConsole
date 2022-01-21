2b10ac9e3606a52f7c3be604c3be5205adde8c61
Rinat Saitov <rsaitov@mail.ru>
2022-01-21 15:58:18 +0300

    Menu and ICommand started

---
+++ SoftwareDeveloperConsole/Auth.cs
@@ -0,0 +1,90 @@
+﻿using RSaitov.SoftwareDevelop.Data;
+using RSaitov.SoftwareDevelop.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RSaitov.SoftwareDevelop.SoftwareDevelopConsole
+{
+    public interface ICommand
+    {
+        object Execute();
+        string Title();
+    }
+    internal class AuthConsoleCommand : ICommand
+    {
+        IService service = new Service(new MockRepository());
+        public object Execute()
+        {
+            Console.Write("Enter your name: ");
+            var user = Console.ReadLine();
+            var worker = service.SelectWorker(user);
+
+            return worker;
+        }
+
+        public string Title() => "Введите имя: ";
+    }
+
+    internal class MainMenu : ICommand
+    {
+        private List<ICommand> _commands { get;  }
+        public MainMenu(IWorker worker)
+        {
+            _commands = new List<ICommand> {
+                new AddWorker(),
+                new ReportAllWorkers(),
+                new Exit(),
+            };
+        }
+        public ICommand GetCommand(string command)
+        {
+            int commandNumber;
+            var result = Int32.TryParse(command, out commandNumber);
+            if (!result)
+                return this;
+            
+            return _commands[commandNumber - 1];
+        }
+        public object Execute()
+        {
+            var menu = string.Join("\r\n", 
+                _commands.Select((x, index) => $"   ({index+1}). {x.Title()}"));
+            return menu;
+        }
+
+        public string Title() => "";
+    }
+
+    internal class AddWorker : ICommand
+    {
+        public object Execute()
+        {
+            return "Adding worker";
+        }
+
+        public string Title() => "Добавить сотрудника";
+    }
+
+    internal class ReportAllWorkers : ICommand
+    {
+        public object Execute()
+        {
+            return "Showing report all workers";
+        }
+
+        public string Title() => "Просмотреть отчет по всем сотрудникам";
+    }
+
+    internal class Exit : ICommand
+    {
+        public object Execute()
+        {
+            Environment.Exit(0);
+            return null;
+        }
+
+        public string Title() => "Выход из программы";
+    }
+}
--- SoftwareDeveloperConsole/Program.cs
+++ SoftwareDeveloperConsole/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using RSaitov.SoftwareDevelop.Data;
+using RSaitov.SoftwareDevelop.Domain;
 
 namespace RSaitov.SoftwareDevelop.SoftwareDevelopConsole
 {
@@ -6,7 +9,39 @@ namespace RSaitov.SoftwareDevelop.SoftwareDevelopConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var isUnauth = true;
+            ICommand authCommand = new AuthConsoleCommand();
+            MainMenu mainMenuCommand = null;
+
+            ICommand nextCommand = authCommand;
+
+            while (nextCommand != null)
+            {
+                if (!ReferenceEquals(null, mainMenuCommand))
+                    Console.WriteLine(mainMenuCommand.Execute());
+
+                var result = nextCommand.Execute();
+
+                if (isUnauth)
+                {
+                    if (ReferenceEquals(null, result))
+                        Console.WriteLine("Worker not found");
+                    else
+                    {
+                        Console.WriteLine($"Welcome, {((IWorker)result).GetName()}!");
+                        mainMenuCommand = new MainMenu((IWorker)result);
+                        nextCommand = mainMenuCommand;
+                        isUnauth = false;
+                    }
+                    continue;
+                }
+
+                Console.WriteLine(result);
+                Console.Write("$ ");
+                var command = Console.ReadLine();
+                nextCommand = mainMenuCommand.GetCommand(command);
+            }
         }
     }
 }
